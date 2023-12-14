2a78407426ea2f46dcd58e84ca8a6579a4364e69
Théo <cobaltium360@gmail.com>
2023-12-14 17:35:18 +0100

    login crée

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import login, authenticate, logout
+from . import forms
 
-# Create your views here.
+def logout(request):
+    logout(request)
+    return redirect('login')
+
+def login_page(request):
+    form = forms.LoginForm()
+    message = ''
+    if request.method == 'POST':
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password'],
+            )
+            if user is not None:
+                login(request, user)
+                message = f'Bonjour, {user.username}! Vous êtes connecté.'
+            else:
+                message = 'Identifiants invalides.'
+    return render(
+        request, 'authentication/login.html', context={'form': form, 'message': message})
+
