ca9b457327dcc3b7ec4ab7cafdf2a423eda0fc00
Théo <cobaltium360@gmail.com>
2023-12-15 17:29:35 +0100

    page flux (galere pour unique ticket enfin trouvé)

--- blog/views.py
+++ blog/views.py
@@ -3,14 +3,25 @@ from django.shortcuts import redirect, render
 from . import forms
 from .models import Ticket
 from users.utils import get_users_viewable_reviews, get_users_viewable_ticket
+from itertools import chain
+from django.db.models import CharField, Value
 
 # Create your views here.
 def flux_page(request):
     reviews = get_users_viewable_reviews(request.user)
+    reviews = reviews.annotate(content_type=Value('REVIEW', CharField()))
     tickets = get_users_viewable_ticket(request.user)
-    print(tickets)
-    # tickets = Ticket.objects.all()
-    return render(request, 'blog/flux.html', {'tickets': tickets})
+    tickets = tickets.annotate(content_type=Value('TICKET', CharField()))
+    unique_tickets = []
+    for ticket in tickets: #check si le ticket a deja une revue si oui on le prend pas en tant que ticket mais en tant que revue 
+        if not ticket.review_set.exists(): #https://docs.djangoproject.com/en/5.0/topics/db/queries/ search for _set
+            unique_tickets.append(ticket)
+    posts = sorted(
+        chain(reviews, unique_tickets), ## merci le cahier des charges xD (a moitié au final mdr)
+        key=lambda post: post.time_created,
+        reverse=True
+    )
+    return render(request, 'blog/flux.html', {'posts': posts})
 
 def ticket_page(request):
     if request.method == 'POST':
